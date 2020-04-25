cc6d11700cdbddfa31c8de9ab3435b4c85da97ad
Xu <972510000@qq.com>
2020-04-25 10:24:46 +0800

    test

--- Example/XModel/XAppDelegate.m
+++ Example/XModel/XAppDelegate.m
@@ -12,8 +12,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+  // Override point for customization after application launch.
+  
+  NSLog(@"123");
+  return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
