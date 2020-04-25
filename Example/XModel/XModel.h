c7450bd244defbca1b0bccfe542c63f31541cda5
Xu <972510000@qq.com>
2020-04-25 10:26:11 +0800

    Test

---
+++ Example/XModel/XModel.h
@@ -0,0 +1,17 @@
+//
+//  XModel.h
+//  XModel_Example
+//
+//  Created by WenQiangXu on 2020/4/25.
+//  Copyright © 2020 972510000@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XModel/Classes/XModel.m
@@ -0,0 +1,13 @@
+//
+//  XModel.m
+//  XModel_Example
+//
+//  Created by WenQiangXu on 2020/4/25.
+//  Copyright © 2020 972510000@qq.com. All rights reserved.
+//
+
+#import "XModel.h"
+
+@implementation XModel
+
+@end
